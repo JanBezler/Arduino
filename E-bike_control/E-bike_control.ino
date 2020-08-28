1a66f19bf20a9facf568f2deaa69400f929495b0
Jan Bezler <jan.filip.bezler@gmail.com>
2020-08-28 17:08:03 +0200

    ebike controller init

---
+++ E-bike_control/E-bike_control.ino
@@ -0,0 +1,19 @@
+
+// input
+const int throttle = A7
+const int turn_left = A6
+const int turn_right = A5
+const int brake = A4
+
+//output
+const int speed_control = A0
+
+void setup() {
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
