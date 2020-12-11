8d3cb01c71ae285d7fe0613bc739e85dbae1b45e
Jan Bezler <jan.filip.bezler@gmail.com>
2020-12-11 11:03:49 +0100

    updated to pwm shift register

--- Omniwheel_car_-_CybAiR_project/Omniwheel_car_-_CybAiR_project.ino
+++ Omniwheel_car_-_CybAiR_project/Omniwheel_car_-_CybAiR_project.ino
@@ -1,3 +1,5 @@
+#include "ShiftRegisterPWM.h"
+
 #define dataPin 10
 #define latchPin 11
 #define clockPin 12
@@ -6,21 +8,82 @@
 #define backHcResponsePin 8
 #define frontHcTriggPin 7
 #define frontHcResponsePin 6
-#define frontHcServoPin 5
+#define frontServoPin 5
 
+const int direcrtionList[11] = {'q','w','e','a','s','d','z','x','c','t','y'};
+const bool dQ[8] = {1,0,0,0,1,0,0,0};
+const bool dW[8] = {1,0,1,0,1,0,1,0};
+const bool dE[8] = {0,0,1,0,0,0,1,0};
+const bool dA[8] = {1,0,0,1,1,0,1,1};
+const bool dS[8] = {0,0,0,0,0,0,0,0};
+const bool dD[8] = {0,1,1,0,0,1,1,0};
+const bool dZ[8] = {0,0,0,1,0,0,0,1};
+const bool dX[8] = {0,1,0,1,0,1,0,1};
+const bool dC[8] = {0,1,0,0,0,1,0,0};
+const bool dR[8] = {1,0,0,1,0,1,1,0};
+const bool dT[8] = {0,1,1,0,1,0,0,1};
+int currentDirection = 's';
+int currentSpeed = 0;
 
-void setup() {
+ShiftRegisterPWM sr(1, 10);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(backHcTriggPin, OUTPUT);
+  pinMode(frontHcTriggPin, OUTPUT);
+  pinMode(frontServoPin, OUTPUT);
+  pinMode(frontHcResponsePin, INPUT);
+  pinMode(backHcResponsePin, INPUT);
+  sr.interrupt(ShiftRegisterPWM::UpdateFrequency::Medium);
 }
 
 
+void goInDirectionPWM()
+{
+  const bool *arr;
+  switch (currentDirection)
+  {
+    case 'q': arr = dQ; break;
+    case 'w': arr = dW; break;
+    case 'e': arr = dE; break;
+    case 'a': arr = dA; break;
+    case 's': arr = dS; break;
+    case 'd': arr = dD; break;
+    case 'z': arr = dZ; break;
+    case 'x': arr = dX; break;
+    case 'c': arr = dC; break;
+    case 'r': arr = dR; break;
+    case 't': arr = dT; break;
+  }
+  
+  for (int i; i < 8;i++)
+  {
+    sr.set(i,*(arr+i)*currentSpeed);
+  }
+  
+}
 
 
+int readInput()
+{
+  int input = Serial.read();
 
-void loop() {
+  for (int i=0; i<11; i++){
+    if (input == direcrtionList[i]) currentDirection = input;
+  }
+  if ((input>='0') && (input<='9')) currentSpeed = input;  
+}
 
 
+void loop() 
+{
+  readInput();
+  goInDirectionPWM();
+
+  delay(10);
 }
