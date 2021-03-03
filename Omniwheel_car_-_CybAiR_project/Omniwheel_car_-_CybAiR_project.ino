8fc57311b1eda4bc6ca2fc524c57323ea7944f33
Jan Bezler <jan.filip.bezler@gmail.com>
2021-03-03 20:54:14 +0100

    Remove bugs and trash

--- Omniwheel_car_-_CybAiR_project/Omniwheel_car_-_CybAiR_project.ino
+++ Omniwheel_car_-_CybAiR_project/Omniwheel_car_-_CybAiR_project.ino
@@ -1,20 +1,35 @@
+#define ShiftRegisterPWM_CUSTOM_INTERRUPT
 #include "ShiftRegisterPWM.h"
+#include <Servo.h>
+
+////////////// PINS ////////////////
 
 #define dataPin 2
 #define clockPin 3
 #define latchPin 4
 
-#define backHcTriggPin 9
-#define backHcResponsePin 8
-#define frontHcTriggPin 7
-#define frontHcResponsePin 6
 #define frontServoPin 5
+#define frontHcTriggPin 6
+#define frontHcEchoPin 7
 
-ShiftRegisterPWM sr(1, 30);
+////////////// VARIABLES ////////////////
+
+#define dfAngle 110
+#define dlAngle dfAngle - 35
+#define drAngle dfAngle + 35
+
+Servo frontServo;
+ShiftRegisterPWM sr(1, 32);
 
 int straightSpeed = 0;
 int turnSpeed = 0;
+bool lookAround = false;
+bool autonomous = false;
+int distDiff = 0;
+
 unsigned short wheels[8] = {0,0,0,0,0,0,0,0};
+
+////////////// SETUP ////////////////
 
 void setup()
 {
@@ -23,58 +38,85 @@ void setup()
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(backHcTriggPin, OUTPUT);
   pinMode(frontHcTriggPin, OUTPUT);
   pinMode(frontServoPin, OUTPUT);
-  pinMode(frontHcResponsePin, INPUT);
-  pinMode(backHcResponsePin, INPUT);
-  sr.interrupt(ShiftRegisterPWM::UpdateFrequency::SuperFast);
+  pinMode(frontHcEchoPin, INPUT);
+  frontServo.attach(frontServoPin);
+  //TIMER2_COMPA_vect
+  cli();
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCNT2  = 0;
+  OCR2A = 38;
+  TCCR2B |= (1 << CS22);
+  TCCR2A |= (1 << WGM21);
+  TIMSK2 |= (1 << OCIE2A);
+  sei();
+  
   for (int i=0;i<8;i++)
   {
     sr.set(i,0);
   }
   Serial.setTimeout(10);
+
 }
+
+ISR(TIMER2_COMPA_vect)
+{
+  cli();
+  sr.update();
+  sei();
+}
+
+////////////// DRIVING WHEELS ////////////////
+
 
 void setWheelsSpeed()
 {
-  short left = straightSpeed + turnSpeed;
-  short right = straightSpeed - turnSpeed;
+  float v = (101-abs(turnSpeed))*(straightSpeed/100) + straightSpeed;
+  float w = (101-abs(straightSpeed))*(turnSpeed/100) + turnSpeed;
+  float right = (v+w)*2.55;
+  float left = (v-w)*2.55;
 
   if (left > 255) left = 255;
   else if (left < -255) left = -255; 
+  
   if (right > 255) right = 255;
   else if (right < -255) right = -255; 
 
   if (left < 0)
   {
-      wheels[3] = left;
-      wheels[5] = left;
-      wheels[2] = 0;
-      wheels[4] = 0;
+    left *= -1;
+    wheels[3] = left;
+    wheels[5] = left;
+    wheels[2] = 0;
+    wheels[4] = 0;
   }
   else
   {
-      wheels[2] = left;
-      wheels[4] = left;
-      wheels[3] = 0;
-      wheels[5] = 0;
+    wheels[2] = left;
+    wheels[4] = left;
+    wheels[3] = 0;
+    wheels[5] = 0;
   }
 
   if (right < 0)
   {
-      wheels[1] = right;
-      wheels[7] = right;
-      wheels[0] = 0;
-      wheels[6] = 0;
+    right *= -1;
+    wheels[1] = right;
+    wheels[7] = right;
+    wheels[0] = 0;
+    wheels[6] = 0;
   }
   else
   {
-      wheels[0] = right;
-      wheels[6] = right;
-      wheels[1] = 0;
-      wheels[7] = 0;
+    wheels[0] = right;
+    wheels[6] = right;
+    wheels[1] = 0;
+    wheels[7] = 0;
   }
+
+  applyChangesOnWheels();
 }
 
 void applyChangesOnWheels()
@@ -85,32 +127,98 @@ void applyChangesOnWheels()
   }
 }
 
+////////////// SENSORS/SERVO ////////////////
+
+int runServo()
+{
+  frontServo.write(drAngle);        
+  delay(300);
+  int dr = getDistance();
+  
+  frontServo.write(dlAngle);
+  delay(500);
+  int dl = getDistance();
+
+  
+  return dl-dr;
+}
+
+float getDistance()
+{
+  digitalWrite(frontHcTriggPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(frontHcTriggPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(frontHcTriggPin, LOW);
+  long duration = pulseIn(frontHcEchoPin, HIGH);
+  return(duration * 0.034 / 2);
+}
+
+////////////// IN/OUT ////////////////
+
 void readInput()
 {
   while(Serial.available() > 0 )
   {
     String str = Serial.readString();
-    Serial.println(str);
-    char typeOfData = str[0];
     String strvalue = "";
+    autonomous = false;
 
-    for (int i=0; i<str.length(); i++)
+
+    for (int i=0; i<=3; i++)
     {
-      if (i>0) strvalue += str[i];
+      if (str[i]!=' ') strvalue += str[i];
     }
-    int value = strvalue.toInt();
+    straightSpeed = strvalue.toInt();
     
-    if(typeOfData == 's') straightSpeed = value;
-    else if (typeOfData == 't') turnSpeed = value;
-   
+    strvalue = "";
+    
+    for (int i=5; i<=str.length(); i++)
+    {
+      if (str[i]!=' ') strvalue += str[i];
+    }
+    turnSpeed = strvalue.toInt();
+    
+    for (int i=0; i<=str.length(); i++)
+    {
+      if (str[i]=='a') autonomous = true;
+      if (str[i]=='s') autonomous = false;
+    }  
   }  
 }
 
+////////////// MAIN LOOP ////////////////
 
 void loop() 
 {
+  
   readInput();
-  setWheelsSpeed();
-  applyChangesOnWheels();
+  lookAround = false;
 
+  if (autonomous)
+  {
+    straightSpeed = 80;
+    turnSpeed = 0;
+    int df = getDistance();
+    Serial.println(df);
+
+    if (df < 25) 
+    {
+      straightSpeed = 0;
+      turnSpeed = 0;
+
+      setWheelsSpeed();
+      delay(300);
+
+      lookAround = true;
+
+      if (distDiff > 0) turnSpeed = 80;
+      else turnSpeed = -80;
+      frontServo.write(dfAngle);
+    }
+  }
+  
+  setWheelsSpeed();
+  if (lookAround) delay(800+random(200));
 }
